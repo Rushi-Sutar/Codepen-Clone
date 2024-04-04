f93b2d2ef49b4e2d6004c5f796030ed7119f2a02
Rushi-Sutar <rushisutar999@gmail.com>
2024-04-04 18:15:23 +0530

    Add Icons And some UI updated

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,5 +1,7 @@
 import { Editor } from "@monaco-editor/react";
 import React, { useEffect, useState } from "react";
+import { FaCss3Alt, FaHtml5 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
 
 function CodeEditor() {
   const [html, setHtml] = useState("<!-- HTML code here -->");
@@ -28,49 +30,63 @@ function CodeEditor() {
   }, [html, css, js]);
   return (
     <>
-    <div className="bg-black text-white min-h-screen py-8 px-4">
-      <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/3 md:pr-4">
-          <h2 className="text-xl font-semibold mb-2">HTML</h2>
-          <Editor
-            height="50vh"
-            defaultLanguage="html"
-            value={html}
-            onChange={(value) => setHtml(value)}
-            theme="vs-dark"
-          />
+      <div className="bg-black text-[#ddd] min-h-screen py-4 px-8">
+        <div className="flex flex-col gap-5 md:flex-row md:gap-0 ">
+          <div className="w-full md:w-1/3">
+            <div className="flex items-center bg-[#1E1E1E] rounded-sm">
+              <FaHtml5 className="text-xl mr-2 ml-4 my-4 fill-orange-400" />
+              <h2 className="text-xl font-semibold mb-2 items-center">HTML</h2>
+            </div>
+
+            <Editor
+              height="50vh"
+              defaultLanguage="html"
+              value={html}
+              onChange={(value) => setHtml(value)}
+              theme="vs-dark"
+            />
+          </div>
+          <div className="w-full md:w-1/3 md:px-2">
+            <div className="flex items-center  bg-[#1E1E1E] rounded-sm">
+              <FaCss3Alt className="text-xl mr-2 ml-4 my-4 fill-cyan-400" />
+              <h2 className="text-xl font-semibold mb-2 items-center">CSS</h2>
+            </div>
+            <Editor
+              height="50vh"
+              defaultLanguage="css"
+              value={css}
+              onChange={(value) => setCss(value)}
+              theme="vs-dark"
+            />
+          </div>
+          <div className="w-full md:w-1/3">
+            <div className="flex items-center bg-[#1E1E1E] rounded-sm">
+              <IoLogoJavascript className="text-xl mr-2 ml-4 my-4 fill-yellow-400" />
+              <h2 className="text-xl font-semibold mb-2 items-center">
+                JavaScript
+              </h2>
+            </div>
+            <Editor
+              height="50vh"
+              defaultLanguage="javascript"
+              value={js}
+              onChange={(value) => setJs(value)}
+              theme="vs-dark"
+            />
+          </div>
         </div>
-        <div className="w-full md:w-1/3 md:px-2">
-          <h2 className="text-xl font-semibold mb-2">CSS</h2>
-          <Editor
-            height="50vh"
-            defaultLanguage="css"
-            value={css}
-            onChange={(value) => setCss(value)}
-            theme="vs-dark"
-          />
-        </div>
-        <div className="w-full md:w-1/3 md:pl-4">
-          <h2 className="text-xl font-semibold mb-2">JavaScript</h2>
-          <Editor
-            height="50vh"
-            defaultLanguage="javascript"
-            value={js}
-            onChange={(value) => setJs(value)}
-            theme="vs-dark"
+        <div className="w-full">
+          <h2 className="text-xl font-semibold mb-2">Preview</h2>
+          <div className="w-full h-[1px] bg-gray-600 mb-2"></div>
+          <iframe
+            id="preview"
+            title="Preview"
+            className="w-full h-screen bg-white rounded-md"
+            style={{ resize: "vertical" }}
           />
         </div>
       </div>
-      <div className="w-full">
-        <h2 className="text-xl font-semibold mb-2">Preview</h2>
-        <iframe
-          id="preview"
-          title="Preview"
-          className="w-full h-64 bg-white"
-        />
-      </div>
-    </div>
-  </>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <nav className="bg-black p-4">
+      <nav className="bg-black p-4 h-14">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
           <div className="flex items-center">
-            <span className="text-white text-xl font-bold">CodePen Clone</span>
+            <span className="text-[#ddd] text-xl font-bold">CodePen Clone</span>
           </div>
         </div>
       </nav>
-      <div className="bg-gray-400 h-[1px]"></div>
+      <div className="bg-gray-800 h-[1px]"></div>
     </>
   );
 }
